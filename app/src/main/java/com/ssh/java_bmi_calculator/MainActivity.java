9569a49dec97daead0fbfe0650f2a89c6591948c
shin-SeungHun <sng7387@gmail.com>
2024-04-17 12:57:50 +0900

    feat: 키와 몸무게를 입력하면 ReulstActivity로 데이터 전달

--- app/src/main/java/com/ssh/java_bmi_calculator/MainActivity.java
+++ app/src/main/java/com/ssh/java_bmi_calculator/MainActivity.java
@@ -1,14 +1,25 @@
 package com.ssh.java_bmi_calculator;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
+import androidx.databinding.DataBindingUtil;
+
+import com.ssh.java_bmi_calculator.databinding.ActivityMainBinding;
+
 
 public class MainActivity extends AppCompatActivity {
+    ActivityMainBinding binding;
+//    MainViewModel viewModel;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -20,5 +31,33 @@ public class MainActivity extends AppCompatActivity {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
         });
+
+        binding = DataBindingUtil.setContentView(this, R.layout.activity_main);
+
+//        viewModel = new ViewModelProvider(this).get(MainViewModel.class);
+        Log.d("main", "create");
+
+        EditText etHeight = binding.etHeight;
+        EditText etWeight = binding.etWeight;
+        Button buttonCalculate = binding.buttonCalculate;
+
+        buttonCalculate.setOnClickListener(v -> {
+            String heightString = etHeight.getText().toString().trim();
+            String weightString = etWeight.getText().toString().trim();
+
+            if (heightString.isEmpty() && weightString.isEmpty()) {
+                Toast.makeText(this, "키와 몸무게를 입력하세요", Toast.LENGTH_SHORT).show();
+            } else {
+                double height = Double.parseDouble(heightString);
+                double weight = Double.parseDouble(weightString);
+
+                Intent intent = new Intent(MainActivity.this, ResultActivity.class);
+                intent.putExtra("height", height);
+                intent.putExtra("weight", weight);
+                startActivity(intent);
+            }
+        });
+
+
     }
 }
